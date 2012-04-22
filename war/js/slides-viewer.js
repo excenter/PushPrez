6ead7e2dc59d25633b9ded5b9c445d3f66bb97f0
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-04-22 17:59:47 +0200

    refacto js next/prev

--- war/js/slides-viewer.js
+++ war/js/slides-viewer.js
@@ -19,17 +19,25 @@ notifyRemote = function(){
 remoteReceive = function(msg){
 	var cmd = jQuery.trim(msg.data);
 	if(cmd == nextToken){
-	    var key = document.createEvent("KeyEvents");
-	    key.initKeyEvent("keydown", true, true, null, false, false, false, false, next_code, next_code);
-//		var key = document.createEvent( 'KeyboardEvent' );
-//		key.initKeyboardEvent( 'keydown', true, true, null, false, false, false, false, 39, 39 );
-	    document.documentElement.dispatchEvent(key);	
+		simulateKey(next_code);
 	} else if(cmd == previousToken){
-	    var key = document.createEvent("KeyEvents");
-	    key.initKeyEvent("keydown", true, true, null, false, false, false, false, prev_code, prev_code);
-	    document.documentElement.dispatchEvent(key);
+		simulateKey(prev_code);
 	}
 }
+
+function coucou(){
+	alert('Coucou');
+}
+
+function simulateKey(code){
+    var key = document.createEvent("KeyEvents");
+    key.initKeyEvent("keydown", true, true, null, false, false, false, false, code, code);
+//	var key = document.createEvent( 'KeyboardEvent' );
+//	key.initKeyboardEvent( 'keydown', true, true, null, false, false, false, false, 39, 39 );
+    document.documentElement.dispatchEvent(key);		
+}
+
+
 
 disconnect = function(){
 	sendMessage(dispatchMessageServlet , cmd(viewerDeconnectionToken));
